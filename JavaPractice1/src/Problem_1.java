619c38deeb37a9f3256eaacf6bfe94fbc89560c6
Nirmita <nirmita931@gmail.com>
2018-01-25 13:13:13 +0545

    new line

--- JavaPractice1/src/Problem_1.java
+++ JavaPractice1/src/Problem_1.java
@@ -4,7 +4,7 @@
 public class Problem_1 {
     
     public static void main (String [] args){
-        
+         System.out.println("Hello\nNirmita KC");
         System.out.println("Hello\nNirmita KC");
     }
 }
